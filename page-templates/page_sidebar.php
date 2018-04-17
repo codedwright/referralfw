c8bc3f8e067b384ad6b70818227b025565daa228
codedwright <info@codedwright.com>
2018-04-17 17:04:58 -0400

    refactor(playlist): begin playlist sidebar module extraction

--- page-templates/page_sidebar.php
+++ page-templates/page_sidebar.php
@@ -1,81 +1,112 @@
 <?php /* Template Name: Sidebar Template */ ?>
-
 <?php get_header(); ?>
-    <?php if(have_posts()) : ?>
-        <?php while(have_posts()) : the_post(); ?>
-            <main>
-            <div class="row">
-                <div class="widget-area col-4 d-none d-lg-block">
-                    <form method="get" action="http://localhost/ReferralFW/">
-                        <div class="row pl-3 pr-3 pb-3">
-                            <input class="col-9 form-control" type="text" value="SEARCH" name="s" id="s" onblur="if (this.value == '')
+<!-- page_sidebar.php -->
+<?php if(have_posts()) : ?>
+<?php while(have_posts()) : the_post(); ?>
+<main class="p-2 p-lg-5">
+    <div class="row">
+        <div class="widget-area col-4 d-none d-lg-block">
+            <form method="get" action="http://localhost/ReferralFW/">
+                <div class="row pl-3 pr-3 pb-3">
+                    <input class="col-9 form-control" type="text" value="SEARCH" name="s" id="s" onblur="if (this.value == '')
                                 {this.value = 'SEARCH';}" onfocus="if (this.value == 'SEARCH')
                                 {this.value = '';}">
-                            <input class="col-2 offset-1 form-control" type="submit" id="searchsubmit" value="OK">
-                        </div>
-                    </form>
-                    <div class="row pb-3">
-                        <div class="col-12">
-                            <h3 class="widget-title lead">Additional Information</h3>
-                            <ul class="list-group">
-                                <?php
-                                    $id = get_the_ID();
-                                    wp_list_pages(array(
-                                        'title_li'    => '',
-                                        'child_of'    => $id,
-                                        'depth'     => '1',
-                                        'walker'      => new Referral_Submenu_Walker()
-                                    ));
-                                ?>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <h3 class="lead">Don’t Take Our Word For It!<br>Listen To Our Clients!</h3>
-                        </div>
-                        <div class="col-12">
-                            <?php
-                                /*
-                                    https://developers.google.com/apis-explorer/#p/
-                                    https://developers.google.com/youtube/player_parameters
-                                */
-                                $playlist = 'PL26AB4782528E64AE';
-                                $url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=" . $playlist . "&key=AIzaSyAnsBJkh_aBM6V0oXAs1zPtuc3t1gnEdcI";
-                                $response = wp_remote_get( $url );
-                                if ( is_array( $response ) ) {
-                                    $header = $response['headers']; // array of http header lines
-                                    $json = $response['body']; // use the content
-                                    $array = json_decode($json, true);
-                                    $length = count($array['items']);
-                                    // echo $length;
-                                    for ($i = 0; $i < $length; $i++) {
-                                        $videoId =  $array['items'][$i]['snippet']['resourceId']['videoId'];
-                                        $title = $array['items'][$i]['snippet']['title'];
-                                        $src = 'https://www.youtube.com/embed/' . $videoId . '?list=' . $playlist . '&controls=0&rel=0&showinfo=0&modestbranding=1';
-                                        echo '<div class="embed-responsive embed-responsive-16by9 pb-1"><iframe class="embed-responsive-item" src="' . $src . '" frameborder="0" allowfullscreen=""></iframe></div><p>' . $title . '</p>';
-                                    }
-                                }
-                            ?>
-                            <!-- <iframe  class="embed-responsive-item" src="https://www.youtube.com/embed/KkF7KZ52ezo?list=PL26AB4782528E64AE&controls=0&rel=0&showinfo=0&modestbranding=1" frameborder="0" allowfullscreen=""></iframe> -->
-                        </div>
-                    </div>
+                    <button class="col-2 offset-1 form-control" type="submit" id="searchsubmit"><i class="fa fa-search"></i></button>
                 </div>
-                <article class="page col-12 col-lg-8 pl-lg-5">
-                    <h1 class="display-4">
-                        <?php the_title(); ?>
-                    </h1>
-                    <?php the_content(); ?>
-                </article>
+            </form>
+            <div class="row pb-3">
+                <div class="col">
+                <?php
+                    global $post;
+                    $children = get_pages( array( 'child_of' => $post->ID ) );
+
+                    if ( is_page() && count( $children ) > 0 ) : ?>
+                    <!-- This is a parent-page (with one or more children) -->
+                    <h3 class="widget-title lead">Additional Information</h3>
+                    <ul class="list-group">
+                        <?php
+                            $id = get_the_ID();
+                            wp_list_pages(array(
+                                'title_li'    => '',
+                                'child_of'    => $id,
+                                'depth'       => '1',
+                                'walker'      => new Referral_Submenu_Walker()
+                            ));
+                        ?>
+                    </ul>
+                    <?php endif; ?>
+                </div>
             </div>
             <div class="row">
-                <div class="widget-area col-12 d-lg-none">
-                    <div class="row">
-                        <div class="col-12">
-                            <h3 class="lead">Don’t Take Our Word For It! Listen To Our Clients!</h3>
-                        </div>
-                        <div class="col-12">
+                <?php
+                    
+                    $meta = wp_get_object_terms( $post->ID,  'playlist' );
+                    if ( ! empty( $meta ) ) {
+                        if ( ! is_wp_error( $meta ) ) {
+                            // echo '<ul>';
+                            //     foreach( $meta as $term ) {
+                            //         echo '<li><a href="' . get_term_meta( $term->term_id, 'playlist-input', true ) . '">' . esc_html( $term->name ) . '</a></li>'; 
+                            //     }
+                            // echo '</ul>';
+                            $playlist = get_term_meta( $meta[0]->term_id, 'playlist-input', true ); ?>
+                            
+                <div class="col">
+                    <h3 class="lead">Don’t Take Our Word For It!<br />Listen To Our Clients!</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                            
+                            
+                            <?php require_once('playlist.php'); playlist($playlist); ?>
+
+                <!-- <iframe  class="embed-responsive-item" src="https://www.youtube.com/embed/KkF7KZ52ezo?list=PL26AB4782528E64AE&controls=0&rel=0&showinfo=0&modestbranding=1" frameborder="0" allowfullscreen=""></iframe> -->
+                </div>
+
                             <?php
+                        }
+                    }
+                ?>
+                    <?php
+                        // /*
+                        //     https://developers.google.com/apis-explorer/#p/
+                        //     https://developers.google.com/youtube/player_parameters
+                        // */
+                        
+                        // $meta = wp_get_object_terms( $post->ID,  'playlist' );
+                        // if ( ! empty( $meta ) ) {
+                        //     if ( ! is_wp_error( $meta ) ) {
+                        //         // echo '<ul>';
+                        //         //     foreach( $meta as $term ) {
+                        //         //         echo '<li><a href="' . get_term_meta( $term->term_id, 'playlist-input', true ) . '">' . esc_html( $term->name ) . '</a></li>'; 
+                        //         //     }
+                        //         // echo '</ul>';
+                        //         $playlist = get_term_meta( $playlist[0]->term_id, 'playlist-input', true );
+                        //     }
+                        // }
+
+                        
+                    ?>
+
+            </div>
+        </div>
+        <article class="page col col-lg-8 pl-lg-5">
+            <h1 class="display-4">
+                <?php the_title(); ?>
+            </h1>
+            <?php the_content(); ?>
+        </article>
+    </div>
+    <div class="row">
+        <div class="widget-area col d-lg-none">
+            <div class="row">
+                <div class="col">
+                    <h3 class="lead">Don’t Take Our Word For It! Listen To Our Clients!</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <?php
                                 $playlist = 'PL26AB4782528E64AE';
                                 $url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=" . $playlist . "&key=AIzaSyAnsBJkh_aBM6V0oXAs1zPtuc3t1gnEdcI";
                                 $response = wp_remote_get( $url );
@@ -98,19 +129,19 @@
                                     }
                                 }
                             ?>
-                        </div>
-                    </div>
-                    <form method="get" action="http://referralfw.com//">
-                        <div class="row pl-3 pr-3">
-                            <input class="col-9 form-control" type="text" value="SEARCH" name="s" id="s" onblur="if (this.value == '')
-                                {this.value = 'SEARCH';}" onfocus="if (this.value == 'SEARCH')
-                                {this.value = '';}">
-                            <input class="col-2 offset-1 form-control" type="submit" id="searchsubmit" value="OK">
-                        </div>
-                    </form>
                 </div>
             </div>
-        </main>
-        <?php endwhile; ?>
-    <?php endif; ?>
+            <form method="get" action="http://referralfw.com//">
+                <div class="row pl-3 pr-3">
+                    <input class="col-9 form-control" type="text" value="SEARCH" name="s" id="s" onblur="if (this.value == '')
+                                {this.value = 'SEARCH';}" onfocus="if (this.value == 'SEARCH')
+                                {this.value = '';}">
+                    <input class="col-2 offset-1 form-control" type="submit" id="searchsubmit" value="OK">
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+<?php endwhile; ?>
+<?php endif; ?>
 <?php get_footer(); ?>
---
+++ page-templates/playlist.php
@@ -0,0 +1,67 @@
+<?php
+// /*
+//     https://developers.google.com/apis-explorer/#p/
+//     https://developers.google.com/youtube/player_parameters
+// */
+                        
+                        
+function playlist($playlist){
+    // $playlist = 'PL26AB4782528E64AE';
+    // https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=PL26AB4782528E64AE&key=AIzaSyAnsBJkh_aBM6V0oXAs1zPtuc3t1gnEdcI
+    $url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=" . $playlist . "&key=AIzaSyAnsBJkh_aBM6V0oXAs1zPtuc3t1gnEdcI";
+    $youtube = array();
+    $nextPageToken = "";
+    do {
+        if ( !empty($nextPageToken) ) {
+            $response = wp_remote_get( $url . "&pageToken=" . $nextPageToken );
+        } else {
+            $response = wp_remote_get( $url);
+        }
+        if ( is_array( $response )  ) {
+            $items = json_decode($response['body'], true)['items'];
+            foreach ($items as $item) {
+                array_push( $youtube, $item );
+            }
+            $nextPageToken = json_decode($response['body'], true)['nextPageToken'];
+        }
+        // var_dump( json_decode($response['body'], true)['items'] );
+    } while ( !is_array( $response ) || json_decode($response['body'], true)['nextPageToken'] );
+    // echo json_encode($youtube);
+    // echo count($youtube['items']);
+    // if ( is_array( $response )  ) {
+    //     $header = $response['headers']; // array of http header lines
+    //     $json = $response['body']; // use the content
+    //     $array = json_decode($json, true);
+    //     $length = count($array['items']);
+    //     // echo $length;
+    //     for ($i = 0; $i < $length; $i++) {
+    //         $videoId =  $array['items'][$i]['snippet']['resourceId']['videoId'];
+    //         $title = $array['items'][$i]['snippet']['title'];
+    //         $src = 'https://www.youtube.com/embed/' . $videoId . '?list=' . $playlist . '&controls=0&rel=0&showinfo=0&modestbranding=1';
+    //         echo '<div class="embed-responsive embed-responsive-16by9 pb-1"><iframe class="embed-responsive-item" src="' . $src . '" frameborder="0" allowfullscreen=""></iframe></div><p>' . $title . '</p>';
+    //     }
+    // }
+    $length = 7;
+    $rand_keys = array_rand($youtube, $length);
+    // echo $youtube[$rand_keys[0]] . "\n";
+    for ($i = 0; $i < $length; $i++) {
+        $videoId =  $youtube[$rand_keys[$i]]['snippet']['resourceId']['videoId'];
+        $title = $youtube[$rand_keys[$i]]['snippet']['title'];
+        $src = 'https://www.youtube.com/embed/' . $videoId . '?list=' . $playlist . '&controls=0&rel=0&showinfo=0&modestbranding=1';
+        echo '<div class="embed-responsive embed-responsive-16by9 pb-1"><iframe class="embed-responsive-item" src="' . $src . '" frameborder="0" allowfullscreen=""></iframe></div><p>' . $rand_keys[$i] . " - " . $title . '</p>';
+    }
+}
+
+// $meta = wp_get_object_terms( $post->ID,  'playlist' );
+// if ( ! empty( $meta ) ) {
+//     if ( ! is_wp_error( $meta ) ) {
+//         // echo '<ul>';
+//         //     foreach( $meta as $term ) {
+//         //         echo '<li><a href="' . get_term_meta( $term->term_id, 'playlist-input', true ) . '">' . esc_html( $term->name ) . '</a></li>'; 
+//         //     }
+//         // echo '</ul>';
+//         $playlist = get_term_meta( $playlist[0]->term_id, 'playlist-input', true );
+//     }
+// }
+
+?>
