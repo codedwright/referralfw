4a76bfe8d3625c673cd4adbaf31aa1f880e7072a
codedwright <info@codedwright.com>
2018-04-27 15:52:11 -0400

    feat: add gallery template

---
+++ gallery.php
@@ -0,0 +1,59 @@
+<?php /* Template Name: Gallery Template */ ?>
+<?php get_header(); ?>
+<!-- page_sidebar.php -->
+<?php if(have_posts()) : ?>
+<?php while(have_posts()) : the_post(); ?>
+<main class="p-2 p-lg-5">
+    <div class="row">
+        <div class="widget-area col-4 d-none d-lg-block">
+            <form method="get" action="http://localhost/ReferralFW/">
+                <div class="row pl-3 pr-3 pb-3">
+                    <?php get_template_part('template-parts/page', 'search'); ?>
+                </div>
+            </form>
+            <div class="row pb-3">
+                <div class="col">
+                    <?php get_template_part('template-parts/page', 'submenu'); ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <h3 class="lead">Don’t Take Our Word For It!<br />Listen To Our Clients!</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">       
+                    <?php get_template_part('template-parts/page', 'playlist'); ?>
+                </div>
+            </div>
+        </div>
+        <article class="page col col-lg-8 pl-lg-5">
+            <h1 class="display-4">
+                <?php the_title(); ?>
+            </h1>
+            <?php the_content(); ?>
+        </article>
+    </div>
+    <div class="row">
+        <div class="widget-area col d-lg-none">
+            <div class="row">
+                <div class="col">
+                    <h3 class="lead">Don’t Take Our Word For It! Listen To Our Clients!</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <?php get_template_part('template-parts/page', 'playlist'); ?>
+                </div>
+            </div>
+            <form method="get" action="http://referralfw.com//">
+                <div class="row pl-3 pr-3">
+                    <?php get_template_part('template-parts/page', 'search'); ?>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
+<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
