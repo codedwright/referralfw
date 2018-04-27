cec984482a53679c1edbcbaf61dc960c5f498513
codedwright <info@codedwright.com>
2018-04-27 15:39:54 -0400

    refactor: reorganize requires, register and enqueue scripts Separated dependencies to enqueue on line 30.

--- functions.php
+++ functions.php
@@ -14,81 +14,20 @@ function referralfw_theme_setup(){
 }
 
 add_action('after_setup_theme', 'referralfw_theme_setup');
-// require_once('referral_faq_meta_box.php');
-require_once('referral_submenu_walker.php');
-require_once('referral_nav_walker.php');
-// require_once('faq_menu_boxes.php');
 
-// function init_widgets($id){
-// 	register_sidebar(array(
-// 		'name'		=> 'YouTube Testimonials',
-// 		'id'		=> 'youtube',
-// 		'before_widget'	=> '<section class="row content-region-1 pt50 pb40"><div class="container"><div class="col-md-12">',
-// 		'after_widget'	=> '</div></div></section>',
-// 		'before_title'	=>	'<h1>',
-// 		'after_title'	=>	'</h1>'
-// 	));
-// }
-//
-// add_action('widgets_init', 'init_widgets');
 
-require_once('playlist-taxonomy.php');
-require_once('referral_faq.php');
-/* 
- * Change Meta Box visibility according to Page Template
- *
- * Observation: this example swaps the Featured Image meta box visibility
- *
- * Usage:
- * - adjust $('#postimagediv') to your meta box
- * - change 'page-portfolio.php' to your template's filename
- * - remove the console.log outputs
- */
+require_once('inc/referral_nav_walker.php');
+require_once('inc/referral_submenu_walker.php');
+require_once('inc/referral_playlist_taxonomy.php');
+require_once('inc/referral_meta_box_dropdown.php');
+require_once('inc/referral_faq_meta_box.php');
+require_once('inc/referral_gallery_generator.php');
 
-add_action('admin_head', 'wpse_50092_script_enqueuer');
-
-function wpse_50092_script_enqueuer() {
-    global $current_screen;
-    if('page' != $current_screen->id) return;
-
-    echo <<<HTML
-        <script type="text/javascript">
-        jQuery(document).ready( function($) {
-
-            /**
-             * Adjust visibility of the meta box at startup
-            */
-            if($('#page_template').val() == 'page-templates/page_sidebar.php') {
-                // show the meta box
-                $('#referral-faq-meta-box').slideDown();
-            } else {
-                // hide your meta box
-                $('#referral-faq-meta-box').slideUp();
-            }
-
-            // Debug only
-            // - outputs the template filename
-            // - checking for console existance to avoid js errors in non-compliant browsers
-            if (typeof console == "object") 
-                console.log ('default value = ' + $('#page_template').val());
-
-            /**
-             * Live adjustment of the meta box visibility
-            */
-            $('#page_template').on('change', function(){
-                    if($(this).val() == 'page-templates/page_sidebar.php') {
-                    // show the meta box
-                    $('#referral-faq-meta-box').slideDown();
-                } else {
-                    // hide your meta box
-                    $('#referral-faq-meta-box').slideUp();
-                }
-
-                // Debug only
-                if (typeof console == "object") 
-                    console.log ('live change value = ' + $(this).val());
-            });                 
-        });    
-        </script>
-HTML;
+function add_theme_scripts() {
+    wp_enqueue_style( 'styles', get_template_directory_uri() . '/assets/css/styles.css');
+    wp_register_script( 'jquery-new', get_template_directory_uri() . '/assets/js/jquery.js', true );
+    wp_register_script( 'popper', get_template_directory_uri() . '/assets/js/popper.js', true );
+    wp_enqueue_script( 'scripts', get_template_directory_uri() . '/assets/js/scripts.js', array ( 'jquery-new', 'popper' ), true );
+    wp_enqueue_script( 'referralfw_scripts', get_template_directory_uri() . '/assets/js/referralfw.js', array ( 'jquery' ));//, array ( 'jquery' ), 1.1, true);
 }
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
