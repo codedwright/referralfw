4ef0d5f8d04d871df0e9cf672bbeb50abdeac5d0
codedwright <info@codedwright.com>
2018-04-30 18:02:34 -0400

    refactor: enqueue scripts after build and minify

--- footer.php
+++ footer.php
@@ -68,18 +68,8 @@
 <?php wp_footer(); ?>
 <script>
     $(document).ready(function(){
-        setHeights = function() {
-            if($(window).width() > 575) {
-                $(".youtube").height($("#slideshow").height());
-                console.log("Window Above 575px");
-            } else if ($("#slideshow").height() < 150) {
-                $(".youtube").height(150);
-                console.log("slideshow Too Small");
-            }
-        }
-        setHeights();
-        $(window).on('resize', setHeights);
-        $('.carousel-item > img').on('load', setHeights);
+        
+        
 
         $(".page_item_has_children > a").after("<span class='dropdown'></span>");
         $(".dropdown").click(function(){
@@ -93,6 +83,15 @@
             $(".offcanvas").removeClass("active");
         });
     });
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-66549127-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-66549127-1');
 </script>
 
 </body>
