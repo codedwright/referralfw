5eeb2082245e1230103250eaec66f62c314d11a8
codedwright <info@codedwright.com>
2018-05-09 11:15:52 -0400

    feat: redirect image attachment pages to home

--- functions.php
+++ functions.php
@@ -15,6 +15,20 @@ function referralfw_theme_setup(){
 
 add_action('after_setup_theme', 'referralfw_theme_setup');
 
+function referralfw_redirect_attachment_page() {
+	if ( is_attachment() ) {
+		global $post;
+		// if ( $post && $post->post_parent ) {
+		// 	wp_redirect( esc_url( get_permalink( $post->post_parent ) ), 301 );
+		// 	exit;
+		// } else {
+			wp_redirect( esc_url( home_url( '/' ) ), 301 );
+			exit;
+		// }
+	}
+}
+add_action( 'template_redirect', 'referralfw_redirect_attachment_page' );
+
 
 require_once('inc/referral_nav_walker.php');
 require_once('inc/referral_submenu_walker.php');
