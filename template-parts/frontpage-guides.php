7d3cce0eeef14945d2b42751bf7806fc9eb78dc1
codedwright <info@codedwright.com>
2018-05-10 16:26:42 -0400

    fix: add links to guides cards

--- template-parts/frontpage-guides.php
+++ template-parts/frontpage-guides.php
@@ -2,9 +2,9 @@
     $data = get_post_meta($post->ID, "referral-frontpage-meta-box", true);
     if(isset($data['guides'])) {
         if($data['guides']['linked'] == 'checked') {
-            $frontpage_guides_link1 = '<a class="d-flex align-items-stretch" href="link">';
-            $frontpage_guides_link2 = '<a class="d-flex align-items-stretch" href="link">';
-            $frontpage_guides_link3 = '<a class="d-flex align-items-stretch" href="link">';        
+            $frontpage_guides_link1 = '<a class="d-flex align-items-stretch" href="http://www.referralfw.com/resource/pdf/Referrals%20Consumer%20Guide%20To%20Carpet%20Cleaning.pdf">';
+            $frontpage_guides_link2 = '<a class="d-flex align-items-stretch" href="http://referralfw.com/wp-content/uploads/Referrals-Consumer-Guide-To-Water-Damage-Restoration.pdf">';
+            $frontpage_guides_link3 = '<a class="d-flex align-items-stretch" href="http://referralfw.com/resources/tips/">';        
             $frontpage_guides_end = '</a>';
         }
         $frontpage_guides_description = $data['guides']['description'];
@@ -39,23 +39,23 @@
                 </form>
             </div>
             <div class="col-lg-9 d-flex align-items-stretch pt-5 px-5 p-lg-0">
-                <div class="row justify-content-center">
+                <div class="row justify-content-center p-3 py-lg-0">
                     <div class="col-sm-4 d-flex p-3 py-lg-0">
-                        $frontpage_guides_link1
+                        $frontpage_guides_link2
                         <div class="card w-100">
-                            <img class="card-img-top" src="http://via.placeholder.com/350x350" alt="Card image cap">
+                            <img class="card-img-top" src="http://referralfw.com/wp-content/uploads/Restoration-Consumer-Guide-Featured-Image.jpg" alt="Card image cap">
                             <div class="card-body d-flex align-items-center justify-content-center">
-                                <h5 class="card-title text-center m-0">Carpet Cleaning<br>Consumer Guide</h5>
+                                <h5 class="card-title text-center m-0">Water Restoration<br>Consumer Guide</h5>
                             </div>
                         </div>
                         $frontpage_guides_end
                     </div>
                     <div class="col-sm-4 d-flex p-3 py-lg-0">
-                        $frontpage_guides_link2
+                        $frontpage_guides_link1
                         <div class="card w-100">
-                            <img class="card-img-top" src="http://via.placeholder.com/350x350" alt="Card image cap">
+                            <img class="card-img-top" src="http://referralfw.com/wp-content/uploads/Carpet-Cleaning-Consumer-Guide-Featured-Image.jpg" alt="Card image cap">
                             <div class="card-body d-flex align-items-center justify-content-center">
-                                <h5 class="card-title text-center m-0">Water Restoration<br>Consumer Guide</h5>
+                                <h5 class="card-title text-center m-0">Carpet Cleaning<br>Consumer Guide</h5>
                             </div>
                         </div>
                         $frontpage_guides_end
@@ -63,7 +63,7 @@
                     <div class="col-sm-4 d-flex pt-3 px-3 p-sm-3 py-lg-0">
                         $frontpage_guides_link3
                         <div class="card w-100">
-                            <img class="card-img-top" src="http://via.placeholder.com/350x350" alt="Card image cap">
+                            <img class="card-img-top" src="http://referralfw.com/wp-content/uploads/Care-Tips-Guide-Featured-Image.jpg" alt="Card image cap">
                             <div class="card-body d-flex align-items-center justify-content-center">
                                 <h5 class="card-title text-center m-0">Care Tips<br>Consumer Guide</h5>
                             </div>
