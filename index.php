dfbb618319dee7db5f52688d8a6818c594af135c
codedwright <info@codedwright.com>
2018-05-10 16:02:01 -0400

    fix: correct layout and comment template

--- index.php
+++ index.php
@@ -1,42 +1,26 @@
-<?php get_header(); ?>
-i
-    <main class="row"><!-- index.php -->
-        <?php if(is_active_sidebar('sidebar')) : ?>
-            <div class="col-md-9">
-        <?php else : ?>
-            <div class="col-md-12">
-        <?php endif; ?>
-        <?php if(have_posts()) : ?>
-            <?php while(have_posts()) : the_post(); ?>
-                <article class="row">
-                    <div class="col-md-12">
-                        <h3><?php the_title(); ?></h3>
-                        <?php the_excerpt(); ?>
-                        <?php if(has_post_thumbnail()): ?>
-                            <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
-                        <?php endif; ?>
-                        <a href="<?php the_permalink(); ?>" class="btn btn-secondary float-right">
-                            <?php echo __('Read More'); ?>
-                        </a>
-                    </div>
-                </article>
-                <div class="clearfix"></div>
-            <?php endwhile; ?>
-        <?php endif; ?>
+<?php get_header(); ?><!-- index.php -->
+<?php if(have_posts()) : ?>
+<div class="bg-light">
+    <main class="bg-light container py-5">
+<?php while(have_posts()) : the_post(); ?>
+        <div class="row">
+            <div class="col-4 p-3">
+                <?php if(has_post_thumbnail()): ?>
+                    <?php the_post_thumbnail('full', array('class' => 'img-fluid')); ?>
+                <?php endif; ?>
             </div>
-        <?php if(is_active_sidebar('sidebar')) : ?>
-            <div class="col-md-3">
-                <?php dynamic_sidebar('sidebar'); ?>
-            </div>
-        <?php endif; ?>
+            <article class="page col p-3 pl-lg-4">
+                <h2>
+                    <?php the_title(); ?>
+                </h2>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn btn-secondary float-right">
+                    <?php echo __('Read More'); ?>
+                </a>
+            </article>
+        </div>
+<?php endwhile; ?>
     </main>
-
-    <?php if(is_active_sidebar('content-region-1')) : ?>
-        <?php dynamic_sidebar('content-region-1'); ?>
-    <?php endif; ?>
-
-    <?php if(is_active_sidebar('content-region-2')) : ?>
-        <?php dynamic_sidebar('content-region-2'); ?>
-    <?php endif; ?>
-
+</div>
+<?php endif; ?>
 <?php get_footer(); ?>
