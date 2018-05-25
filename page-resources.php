bb90a01bd205fcc11b8f88922c32467ddefe8089
codedwright <info@codedwright.com>
2018-05-25 09:28:50 -0400

    feat: add resources template

---
+++ page-resources.php
@@ -0,0 +1,187 @@
+<?php get_header(); ?><!-- New Client -->
+<?php if(have_posts()) : ?>
+<?php while(have_posts()) : the_post(); ?>
+<div class="bg-light">
+    <main class="bg-light container py-5">
+        <div class="row">
+            <article class="page col p-3 pr-lg-4">
+                <div class="jumbotron p-3 p-md-5 text-light rounded bg-dark">
+                    <div class="col-md-6 px-0">
+                        <h1 class="display-4 font-italic">
+                            Resources
+                        </h1>
+                        <p class="lead my-3">
+                            We are strong believers that education and knowledge are tools to success. We want to put that tool in your hands with the following valuable resources.
+                        </p>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/faq/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Frequently Asked Questions
+                                    </h3>
+                                    <p class="card-text">
+                                        Choosing a service professional can be confusing sometimes. There are so many details to consider. We have gathered up our most frequent questions and answers for your convenience.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/preparation/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Cleaning Preparation Checklist
+                                    </h3>
+                                    <p class="card-text">
+                                        So you've scheduled your cleaning appointment. Now what? We've put together a handy checklist of preparation tasks you can perform in order to get the best clean.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/post-cleaning-drying-tips/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Post Cleaning Drying Tips
+                                    </h3>
+                                    <p class="card-text">
+                                        One main concern we hear from our clients is "how long will it take to dry?". Here are some tips for continuing the speed drying process after the technicians leave.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/post-cleaning-drying-tips/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Carpet Cleaning Consumer Guide
+                                    </h3>
+                                    <p class="card-text">
+                                        The best decisions are based on a foundation of educational research. We've put together a complete guide to choosing a professional carpet cleaner to educate you on the basics of carpet cleaning.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/spotting/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Carpet Spotting Guide
+                                    </h3>
+                                    <p class="card-text">
+                                        Accidents happen, resulting in spots and stains on our carpet. The likelihood of spotting is even higher if you have children or pets. Here is a handy guide just for those time.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/tips/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Carpet Care Tips
+                                    </h3>
+                                    <p class="card-text">
+                                        You've invested a large sum of money into your carpeting. That investment deserves the best of care to prolong it's return. This list of tips will give your carpet longevity.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <a class="card-link" href="#">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Water Restoration Guide
+                                    </h3>
+                                    <p class="card-text">
+                                        We never hope for it, but sometimes flodding happens in homes. What should you do if you find water in your basement or home? Read this guide to learn what to do in such an event.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/links/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Recommended Ethical Services
+                                    </h3>
+                                    <p class="card-text">
+                                        We offer the very best in cleaning and restoration, but we don't do everything. Sometimes you may need services we do not offer. This list includes service professionals we would recommend.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-md-6">
+                        <a class="card-link" href="https://referralfw.com/resources/links/">
+                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                                <div class="card-body d-flex flex-column justify-content-center">
+                                    <h3 class="mb-0 text-dark">
+                                        Things To Do In Fort Wayne
+                                    </h3>
+                                    <p class="card-text">
+                                        We are proud of our community and all the events and activities avaiable. We put together this list of resources here in Fort Wayne for your convience.
+                                    </p>
+                                </div>
+                                <div class="m-0">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </main>
+</div>
+<?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
