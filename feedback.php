5531d080640ed3676e0157bab648f8f3c35e539e
codedwright <info@codedwright.com>
2018-06-05 12:29:02 -0400

    fix(form action): change form action to php include

--- feedback.php
+++ feedback.php
@@ -11,7 +11,7 @@ get_header(); ?><!-- Feedback (look below) -->
     <main class="bg-light container py-5">
         <div class="row">
             <article class="page col p-3 pr-lg-4">
-                <form action="https://referralfw.com/contact/review/" method="post">
+                <form action="<?php echo get_template_directory_uri(); ?>/inc/referral_feedback_mail.php" method="post">
                 <?php 
                     wp_nonce_field( basename( __FILE__ ), 'referral_feedback_nonce' );
                     if(get_permalink() =='https://referralfw.com/contact/restorationfeedback/') {
