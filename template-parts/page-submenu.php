21763722b9c6224d58ebfbda7fdc41892e5bfc32
codedwright <info@codedwright.com>
2018-06-07 17:15:45 -0400

    feat: include search for ancestors up two levels for grandchildren

--- template-parts/page-submenu.php
+++ template-parts/page-submenu.php
@@ -2,8 +2,7 @@
 
 global $post;
 $children = get_pages( array( 'child_of' => $post->ID ) );
-
-if ( is_page() && count( $children ) > 0 ) : ?>
+if ( ( is_page() && count( $children ) > 0 ) ) : ?>
 <!-- This is a parent-page (with one or more children) -->
 <h3 class="lead text-center text-uppercase">Additional Information</h3>
 <ul class="list-group text-uppercase" style="font-weight: 400;">
@@ -12,6 +11,23 @@ if ( is_page() && count( $children ) > 0 ) : ?>
         wp_list_pages(array(
             'title_li'    => '',
             'child_of'    => $id,
+            'depth'       => '1',
+            'walker'      => new Referral_Submenu_Walker()
+        ));
+    ?>
+</ul>
+<?php endif;
+$ancestors = get_ancestors( $post->ID, 'page' );
+if ( is_page() && count( get_ancestors( $post->ID, 'page' ) ) > 1 ) : 
+?>
+<!-- This is a parent-page (with one or more children) -->
+<h3 class="lead text-center text-uppercase">Additional Information</h3>
+<ul class="list-group text-uppercase text-dark" style="font-weight: 400;">
+    <?php
+        $id = get_the_ID();
+        wp_list_pages(array(
+            'title_li'    => '',
+            'child_of'    => (string)$ancestors[0],
             'depth'       => '1',
             'walker'      => new Referral_Submenu_Walker()
         ));
