794ddca3ba725700b37417f3d8b4c3ea58a93114
codedwright <info@codedwright.com>
2018-06-07 17:13:32 -0400

    feat: remove editor on specific templates

--- inc/referral_meta_box_dropdown.php
+++ inc/referral_meta_box_dropdown.php
@@ -22,19 +22,24 @@ function referral_meta_box_dropdown() {
 
             /**
              * Adjust visibility of the meta box at startup
+             * $('#postdivrich').slideUp() - Editor
+             * #normal-sortables > [id^=referral-]:not(#referral-faq-meta-box) - All Other Meta Boxes
             */
             if($('#page_template').val() == 'faq.php') {
                 // show the meta box
                 $('#referral-faq-meta-box').slideDown();
                 $('#normal-sortables > [id^=referral-]:not(#referral-faq-meta-box)').slideUp();
+                $('#postdivrich').slideUp();
             } else if($('#page_template').val() == 'team.php') {
                 // show the meta box
                 $('#referral-team-meta-box').slideDown();
                 $('#normal-sortables > [id^=referral-]:not(#referral-team-meta-box)').slideUp();
+                $('#postdivrich').slideUp();
             } else if($('#page_template').val() == 'frontpage.php') {
                 // show the meta box
                 $('#referral-frontpage-meta-box').slideDown();
                 $('#normal-sortables > [id^=referral-]:not(#referral-frontpage-meta-box)').slideUp();
+                $('#postdivrich').slideUp();
             } else if($('#page_template').val() == 'gallery.php') {
                 // show the meta box
                 $('#referral-gallery-meta-box').slideDown();
@@ -57,15 +62,18 @@ function referral_meta_box_dropdown() {
                 if($(this).val() == 'faq.php') {
                     // show the meta box
                     $('[id^=referral-faq-meta-box]').slideDown();
+                    $('#postdivrich').slideUp();
                 } else if($(this).val() == 'team.php') {
                     // show the meta box
                     $('[id^=referral-team-meta-box]').slideDown();
+                    $('#postdivrich').slideUp();
                 } else if($(this).val() == 'gallery.php') {
                     // show the meta box
                     $('#referral-gallery-meta-box').slideDown();
                 } else if($(this).val() == 'frontpage.php') {
                     // show the meta box
                     $('#referral-frontpage-meta-box').slideDown();
+                    $('#postdivrich').slideUp();
                 } else {
                     // hide your meta box
                     $('#normal-sortables > [id^=referral-]').slideUp();
