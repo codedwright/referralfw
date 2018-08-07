d1790e28db83ef2de62d4942f3483e12614f7e75
codedwright <info@codedwright.com>
2018-08-07 14:24:29 -0400

    chore: restructure text

--- page-about.php
+++ page-about.php
@@ -20,12 +20,16 @@
                         <a class="card-link" href="https://referralfw.com/about/history/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">Our History</h3>
-                                    <p class="card-text text-dark">Referral started as an entrepreneurial adventure out of the back of Ron's El Camino, back when big hair and mustaches
-                                        were the in thing. His first client? Denny's restaurant.</p>
+                                    <h3 class="mb-0 text-dark">
+                                        Our History
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        Referral started as an entrepreneurial adventure out of the back of Ron's El Camino, back when big hair and mustaches
+                                        were the in thing. His first client? Denny's restaurant.
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Our-History.jpg">
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Our-History.jpg" />
                                 </div>
                             </div>
                         </a>
@@ -34,12 +38,16 @@
                         <a class="card-link" href="https://referralfw.com/about/team/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">The Referral Team</h3>
-                                    <p class="card-text text-dark">Our team is made up of individuals who are committed to making cheerleaders for the company. That means you will
-                                        always be treated with respect by knowledgeable and friendly staff.</p>
+                                    <h3 class="mb-0 text-dark">
+                                        The Referral Team
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        Our team is made up of individuals who are committed to making cheerleaders for the company. That means you will
+                                        always be treated with respect by knowledgeable and friendly staff.
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Referral-Team.jpg" />
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Referral-Team.jpg" >
                                 </div>
                             </div>
                         </a>
@@ -50,13 +58,16 @@
                         <a class="card-link" href="https://referralfw.com/about/photo-gallery/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">Photo Galleries</h3>
-                                    <p class="card-text text-dark">Sometimes you just have to see it to believe it. We have provided cleaning and restoration services for a
-                                        wide variety of clients, experiencing a diversity of situations.</p>
+                                    <h3 class="mb-0 text-dark">
+                                        Photo Galleries
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        Sometimes you just have to see it to believe it. We have provided cleaning and restoration services for a
+                                        wide variety of clients, experiencing a diversity of situations.
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Photo-Gallery.jpg"
-                                    />
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Photo-Gallery.jpg" >
                                 </div>
                             </div>
                         </a>
@@ -65,12 +76,16 @@
                         <a class="card-link" href="https://referralfw.com/about/community/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">Community Support</h3>
-                                    <p class="card-text text-dark">We believe in supporting our community. It is, after all, our home. That's why we take time to invest in
-                                        local businesses and charities. Take a look at what we've been up to.</p>
+                                    <h3 class="mb-0 text-dark">
+                                        Community Support
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        We believe in supporting our community. It is, after all, our home. That's why we take time to invest in
+                                        local businesses and charities. Take a look at what we've been up to.
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Community.jpg" />
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Community.jpg" >
                                 </div>
                             </div>
                         </a>
@@ -81,13 +96,16 @@
                         <a class="card-link" href="https://referralfw.com/about/careers/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">Career Opportunities</h3>
-                                    <p class="card-text text-dark">Do you have ambition and a willingness to learn? Perhaps this is the place for you. With benefits and a pleasent
-                                        team atmosphere, working with Referral is a pleasant opportunity.</p>
+                                    <h3 class="mb-0 text-dark">
+                                        Career Opportunities
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        Do you have ambition and a willingness to learn? Perhaps this is the place for you. With benefits and a pleasent
+                                        team atmosphere, working with Referral is a pleasant opportunity.
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Career.jpg"
-                                    />
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Career.jpg" >
                                 </div>
                             </div>
                         </a>
@@ -96,13 +114,16 @@
                         <a class="card-link" href="https://referralfw.com/about/trust/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">Why Trust Us</h3>
-                                    <p class="card-text text-dark">Advertising and web content can all sound good, but how do you know if you can truly trust a company? We
-                                        have a few reasons why we think you will find us trustworthy.</p>
+                                    <h3 class="mb-0 text-dark">
+                                        Why Trust Us
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        Advertising and web content can all sound good, but how do you know if you can truly trust a company? We
+                                        have a few reasons why we think you will find us trustworthy.
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Why-Trust-Referral.jpg"
-                                    />
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Why-Trust-Referral.jpg" >
                                 </div>
                             </div>
                         </a>
@@ -113,26 +134,16 @@
                         <a class="card-link" href="https://referralfw.com/about/blog/">
                             <div class="card flex-md-row mb-4 box-shadow h-md-250">
                                 <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">Referral's Blog</h3>
-                                    <p class="card-text text-dark">This is kind of the newsy part of our website. We tell you what's going on in the company, in the industry,
-                                        and in town. Audrey and Alan have also started their own show, so don't miss those!</p>
+                                    <h3 class="mb-0 text-dark">
+                                        Referral's Blog
+                                    </h3>
+                                    <p class="card-text text-dark">
+                                        This is kind of the newsy part of our website. We tell you what's going on in the company, in the industry,
+                                        and in town. Audrey and Alan have also started their own show, so don't miss those!
+                                    </p>
                                 </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Blog.jpg"
-                                    />
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-md-6 d-none">
-                        <a class="card-link" href="https://referralfw.com/about/history/">
-                            <div class="card flex-md-row mb-4 box-shadow h-md-250">
-                                <div class="card-body d-flex flex-column justify-content-center">
-                                    <h3 class="mb-0 text-dark">The Referral Team</h3>
-                                    <p class="card-text text-dark">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                                </div>
-                                <div class="m-0">
-                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://via.placeholder.com/200x250" />
+                                <div class="m-0 d-none d-lg-block" style="width:200px;">
+                                    <img class="card-img-right flex-auto d-none d-lg-block" src="https://referralfw.com/wp-content/uploads/Blog.jpg" >
                                 </div>
                             </div>
                         </a>
