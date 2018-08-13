c275b8408e41d0025889f0c837023e953723ae7b
ItsBrenAgain <39308151+ItsBrenAgain@users.noreply.github.com>
2018-08-13 14:48:13 -0400

    Update page-newclient.php

--- page-newclient.php
+++ page-newclient.php
@@ -5,18 +5,38 @@
     <main class="bg-light container py-5">
         <div class="row">
             <article class="content col p-3 pr-lg-4">
-                <div class="row align-items-center">
+				<div class="row">
+					<h2>New Client Savings</h2>
+				</div> 
+				<div class="row align-items-center">
                     <div class="col-md-4">
                         <figure class="figure w-100">
-                            <img src="https://referralfw.com/wp-content/uploads/New-Client-Coupon-Image-Square.jpg" alt="New Client Gift Card" class="figure-img img-fluid rounded p-3">
+                            <img src="https://referralfw.com/wp-content/uploads/New-Client-Coupon-Image-SQ.jpg" alt="New Client Gift Card" class="figure-img img-fluid rounded p-3">
                         </figure>
                     </div>
-                    <div class="col-md-8 text-center">
-                        <p class="h4 font-weight-light">At Referral, we treat every client with respect and cheerfulness. We think that after just one Referral
+                    <div class="col-md-8">
+                        <p class="h5 font-weight-normal">At Referral, we treat every client with respect and cheerfulness. We think that after just one Referral
                             experience, you will happily join the thousands of other Referral cheerleaders in the Fort Wayne
                             area. As a token of our appreciation, please print this $25 gift card, redeemable during your
                             first Referral experience.</p><br>
-                        <a href="https://referralfw.com/wp-content/uploads/New-Client-Coupon.pdf" class="btn btn-outline-primary text-uppercase" target="_blank">Print Gift Card</a>
+                        <center><a href="https://referralfw.com/wp-content/uploads/New-Client-Coupon.pdf" class="btn btn-primary text-uppercase" target="_blank">Print Gift Card</a></center>
+                    </div>
+                </div>
+				                <div class="row">
+					<h2>Loyal Client Savings</h2>
+				</div> 
+				<div class="row align-items-center">
+                    <div class="col-md-4">
+                        <figure class="figure w-100">
+                            <img src="https://referralfw.com/wp-content/uploads/Referral-Reward-Image-SQ.jpg" alt="New Client Gift Card" class="figure-img img-fluid rounded p-3">
+                        </figure>
+                    </div>
+                    <div class="col-md-8">
+                        <p class="h5 font-weight-normal">At Referral, we treat every client with respect and cheerfulness. We think that after just one Referral
+                            experience, you will happily join the thousands of other Referral cheerleaders in the Fort Wayne
+                            area. As a token of our appreciation, please print this $25 gift card, redeemable during your
+                            first Referral experience.</p><br>
+                        <center><a href="https://referralfw.com/wp-content/uploads/New-Client-Coupon.pdf" class="btn btn-primary text-uppercase" target="_blank">Print Gift Card</a></center>
                     </div>
                 </div>
                 <hr class="py-md-3">
