98d1ad2edd94534c078a45685efaa4fd648aa8bd
ItsBrenAgain <39308151+ItsBrenAgain@users.noreply.github.com>
2018-08-17 14:38:54 -0400

    Update page-newclient.php

--- page-newclient.php
+++ page-newclient.php
@@ -38,10 +38,14 @@
                     <div class="col-md-4">
                         <figure class="figure w-100">
                             <img src="https://referralfw.com/wp-content/uploads/Referral-Reward-Image-SQ.jpg" alt="Referral Carpet Cleaning Coupon" class="figure-img img-fluid rounded p-3">
+							<img src="https://referralfw.com/wp-content/uploads/WaterDamage-Referral-Reward-SQ.jpg" alt="Water Referral Carpet Cleaning Coupon" class="figure-img img-fluid rounded p-3">
                         </figure>
                     </div>
                     <div class="col-md-8 pt-3">
-                        <p class="h5 font-weight-normal">If you're a current client, you know that our service is top notch and our cleaning methods outshine the competitors. But did you know that we actually give you credit for telling your friends and family about our services? For every person you refer who also schedules a job, you will receive a $25 credit toward your next cleaning. The best part? There's no limit to the number of referral rewards you can pile up. So go ahead! Start your very own referral reward collection. You could easily end up with free cleaning services.</p>
+						<p class="h5 font-weight-normal">If you're a current client, you know that our service is top notch and our cleaning methods outshine the competitors. But did you know that we actually give you credit for telling your friends and family about our services?</p> 
+						<p class="h5 font-weight-normal">For every person you refer who also schedules a cleaning job, you will receive a $25 credit toward any of our services.</p> 
+						<p class="h5 font-weight-normal">When you refer a water damage emergency, we will reward you with a $50 credit toward any of our services.</p>
+						<p class="h5 font-weight-normal">The best part? There's no limit to the number of referral rewards you can pile up. So go ahead! Start your very own referral reward collection. You could easily end up with free cleaning services.</p>
 						<p><strong><em>There are three ways to refer others to our company:</em></strong></p>
 						<ol>
   							<li>Verbally pass on reccomendations to anyone. We always ask how new clients hear about us, so just tell them to mention your name when they schedule.</li>
@@ -49,7 +53,7 @@
   							<li>Visit our Refer a Friend page, and list names and addresses there. We will send them the same letter with a special savings coupon as a gift to them from you.</li>
 						</ol>
 						<p><strong>Pssssst... Don't forget to reccomend us to the one in charge of building maintenace at your workplace. Commercial floor cleaning is one of our specialities, and having a clean work enviroment is a benefit to you and all your fellow employees.</strong></p><br>
-                        <center><a href="https://referralfw.com/wp-content/uploads/New-Client-Coupon.pdf" class="btn btn-primary text-uppercase" target="_blank">Refer a Friend</a></center>
+                        <center><a href="https://referralfw.com/contact/refer/" class="btn btn-primary text-uppercase" target="_blank">Refer a Friend</a></center>
                     </div>
                 </div>
 			<hr>
@@ -64,10 +68,11 @@
                     <div class="col-md-4">
                         <figure class="figure w-100">
                             <img src="https://referralfw.com/wp-content/uploads/Review-Reward-SQ.jpg" alt="Online Review Carpet Cleaning Coupon" class="figure-img img-fluid rounded p-3">
+							
                         </figure>
                     </div>
                     <div class="col-md-8 pt-3">
-                        <p class="h5 font-weight-normal"> If you're a current cleint, would you mind giving us a little shout out on social media? We'd greatly appreciate it. In fact, we appreciate it so much, we'll give you a $10 credit for each platform where you leave a review. Check out all the different platforms where you can shout the praises of Referral.</p>
+                        <p class="h5 font-weight-normal"> If you're a current client, would you mind giving us a little shout out on social media? We'd greatly appreciate it. In fact, we appreciate it so much, we'll give you a $10 credit for each platform where you leave a review. Check out all the different platforms where you can shout the praises of Referral.</p>
 						<br>
 						<center><a href="https://referralfw.com/contact/review/" class="btn btn-primary text-uppercase" target="_blank">Leave Social Media Reviews</a></center>				
 					</div>
@@ -84,10 +89,13 @@
                     <div class="col-md-4">
                         <figure class="figure w-100">
                             <img src="https://referralfw.com/wp-content/uploads/Newsletter-Savings-SQ.jpg" alt="Newsletter Carpet Cleaning Coupon" class="figure-img img-fluid rounded p-3">
+							
                         </figure>
                     </div>
                     <div class="col-md-8 pt-3">
                         <p class="h5 font-weight-normal">Our newsletter subscribers have access to monthly specials and coupons. We are working on a way for you to sign up right here on our website, but for now, if you are not already receiving our newsletters and would like to, give us a quick call, and we will add you to the list.</p>
+							<br>
+						<p class="h5 font-weight-normal">We also occasionally send out postcards to those on our mailing list. These postcards generally have a special offer listed on them.</p>
 
 					</div>
 				</div>
